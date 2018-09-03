9fc161774ed2e2529121be620cee91df23ce1f2e
MostafaGohar <mostafa-gohar@hotmail.com>
2018-09-03 19:34:09 +0200

    added slide layout and grid layout to main

--- BachWTC2/app/src/main/java/gohar/mostafa/bachwtc/MainActivity.java
+++ BachWTC2/app/src/main/java/gohar/mostafa/bachwtc/MainActivity.java
@@ -1,35 +1,110 @@
 package gohar.mostafa.bachwtc;
 
-import android.app.Application;
-import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b1 = findViewById(R.id.button1);
-        Button b2 = findViewById(R.id.button2);
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(getApplicationContext(), BookActivity.class);
-                myIntent.putExtra("BOOK_NUMBER", 1);
-                startActivity(myIntent);
-            }
-        });
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
 
-        b2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(getApplicationContext(), BookActivity.class);
-                myIntent.putExtra("BOOK_NUMBER", 2);
-                startActivity(myIntent);
-            }
-        });
+
+        ViewPager mViewPager;
+        SlidingTabLayout tabs;
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true);
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(mViewPager);
     }
+
+    class MyPagerAdapter extends FragmentPagerAdapter{
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            MyFragment myFragment = MyFragment.getInstance(position);
+            return myFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Book "+position;
+        }
+    }
+
+    // Instances of this class are fragments representing a single
+    // object in our collection.
+    public static class MyFragment extends Fragment {
+        public static MyFragment getInstance(int position){
+            MyFragment myFragment = new MyFragment();
+            Bundle args = new Bundle();
+            args.putInt("position",position);
+            myFragment.setArguments(args);
+            return myFragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+            View layout = inflater.inflate(R.layout.songs_page,container,false);
+
+            GridLayout mGridLayout = layout.findViewById(R.id.gridLayout);
+            mGridLayout.setRowCount(6);
+            mGridLayout.setColumnCount(4);
+
+            Bundle bundle = getArguments();
+            int bookNumber = bundle.getInt("position");
+
+            for(int i = 0;i<6;i++){
+                for(int j = 0;j<4;j++){
+                    ImageView imageView = new ImageView(getContext());
+                    if(j%2 == 0)
+                        imageView.setImageResource(R.mipmap.ic_launcher);
+                    else
+                        imageView.setImageResource(R.mipmap.ic_launcher_round);
+
+
+                    GridLayout.LayoutParams parem = new GridLayout.LayoutParams(GridLayout.spec(GridLayout.UNDEFINED, 1f),      GridLayout.spec(GridLayout.UNDEFINED, 1f));
+                    imageView.setLayoutParams(parem);
+
+                    mGridLayout.addView(imageView);
+                }
+            }
+            return layout;
+        }
+    }
+
 }
