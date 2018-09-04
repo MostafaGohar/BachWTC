b47c4c5abc0783c62afd68303a5d898b3742f7e1
MostafaGohar <mostafa-gohar@hotmail.com>
2018-09-04 13:38:14 +0200

    added key signatures and names of songs

--- BachWTC2/app/src/main/java/gohar/mostafa/bachwtc/MainActivity.java
+++ BachWTC2/app/src/main/java/gohar/mostafa/bachwtc/MainActivity.java
@@ -1,5 +1,7 @@
 package gohar.mostafa.bachwtc;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,12 +11,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayout;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -82,29 +88,84 @@ public class MainActivity extends AppCompatActivity {
             View layout = inflater.inflate(R.layout.songs_page,container,false);
 
             GridLayout mGridLayout = layout.findViewById(R.id.gridLayout);
-            mGridLayout.setRowCount(6);
+            mGridLayout.setRowCount(12);
             mGridLayout.setColumnCount(4);
 
             Bundle bundle = getArguments();
             int bookNumber = bundle.getInt("position");
 
             for(int i = 0;i<6;i++){
+                ArrayList<String> songNames = new ArrayList<>();
                 for(int j = 0;j<4;j++){
                     ImageView imageView = new ImageView(getContext());
-                    if(j%2 == 0)
-                        imageView.setImageResource(R.mipmap.ic_launcher);
-                    else
-                        imageView.setImageResource(R.mipmap.ic_launcher_round);
+
+                    int x = (j+1) + (4*i);
 
 
-                    GridLayout.LayoutParams parem = new GridLayout.LayoutParams(GridLayout.spec(GridLayout.UNDEFINED, 1f),      GridLayout.spec(GridLayout.UNDEFINED, 1f));
-                    imageView.setLayoutParams(parem);
+                    imageView.setImageResource(getKeySignatureDrawable(getContext(),x,bookNumber));
+
+                    GridLayout.LayoutParams param = new GridLayout.LayoutParams(GridLayout.spec(GridLayout.UNDEFINED, 1f),      GridLayout.spec(GridLayout.UNDEFINED, 1f));
+                    imageView.setLayoutParams(param);
 
                     mGridLayout.addView(imageView);
+                    songNames.add(getSongName(x, bookNumber));
                 }
+                for(int j = 0;j<4;j++){
+                    TextView textView = new TextView(getContext());
+                    textView.setText(songNames.get(j));
+                    textView.setGravity(Gravity.CENTER);
+                    textView.setTextColor(Color.BLACK);
+
+                    GridLayout.LayoutParams param = new GridLayout.LayoutParams(GridLayout.spec(GridLayout.UNDEFINED, 0.1f),      GridLayout.spec(GridLayout.UNDEFINED, 1f));
+                    textView.setLayoutParams(param);
+                    mGridLayout.addView(textView);
+                }
+
             }
             return layout;
         }
+    }
+
+    public static int getKeySignatureDrawable(Context context, int x, int book){
+
+        int id;
+        if(book == 0 && x == 8)
+            id = context.getResources().getIdentifier("joined", "drawable", context.getPackageName());
+        else
+            id = context.getResources().getIdentifier("ic_"+x, "drawable", context.getPackageName());
+        return id;
+    }
+
+    public static String getSongName(int x, int book){
+        switch(x){
+
+            case 1:return "C major";
+            case 2:return "C minor";
+            case 3:return "C# major";
+            case 4:return "C# minor";
+            case 5:return "D major";
+            case 6:return "D minor";
+            case 7:return "E♭ major";
+            case 8: if(book == 0) return "E♭ minor \n/ D# minor"; else return "D# minor";
+            case 9:return "E major";
+            case 10:return "E minor";
+            case 11:return "F major";
+            case 12:return "F minor";
+            case 13:return "F# major";
+            case 14:return "F# minor";
+            case 15:return "G major";
+            case 16:return "G minor";
+            case 17:return "A♭ major";
+            case 18:return "G# minor";
+            case 19:return "A major";
+            case 20:return "A minor";
+            case 21:return "B♭ major";
+            case 22:return "B♭ minor";
+            case 23:return "B major";
+            case 24:return "B minor";
+
+        }
+        return ""+x;
     }
 
 }
