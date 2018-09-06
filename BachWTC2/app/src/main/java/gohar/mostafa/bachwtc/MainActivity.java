40c1d87ce5dcabeb2210a9633ede1065a64c95b9
MostafaGohar <mostafa-gohar@hotmail.com>
2018-09-06 16:26:34 +0200

    added pdfs of book 2

--- BachWTC2/app/src/main/java/gohar/mostafa/bachwtc/MainActivity.java
+++ BachWTC2/app/src/main/java/gohar/mostafa/bachwtc/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -36,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
 
@@ -73,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return "Book "+position;
+            return "Book "+(position+1);
         }
     }
 
@@ -97,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
             mGridLayout.setColumnCount(4);
 
             Bundle bundle = getArguments();
-            int bookNumber = bundle.getInt("position");
+            int bookNumber = bundle.getInt("position")+1;
 
             for(int i = 0;i<6;i++){
                 for(int j = 0;j<4;j++){
@@ -109,13 +111,14 @@ public class MainActivity extends AppCompatActivity {
                     LinearLayout l2 = new LinearLayout((getContext()));
                     linearLayout.setOrientation(LinearLayout.VERTICAL);
                     ImageView imageView = new ImageView(getContext());
-                    TextView textView = new TextView(getContext());
+                    AutoResizeTextView textView = new AutoResizeTextView(getContext());
 
                     imageView.setImageResource(getKeySignatureDrawable(getContext(),x,bookNumber));
 
                     textView.setText(getSongName(x, bookNumber));
                     textView.setGravity(Gravity.CENTER);
                     textView.setTextColor(Color.BLACK);
+                    textView.setTypeface(Typeface.DEFAULT_BOLD);
 
                     l1.addView(imageView);
                     l2.addView(textView);
@@ -147,7 +150,16 @@ public class MainActivity extends AppCompatActivity {
                     linearLayout.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-                            Log.v("hey","yes"+linearLayout.getTag());
+
+                            int bookNumber = Integer.parseInt(linearLayout.getTag().toString().split("_")[0]);
+                            int songNumber = Integer.parseInt(linearLayout.getTag().toString().split("_")[1]);
+                            Log.v("hey","yes"+bookNumber+"_"+songNumber);
+                            Intent intent = new Intent(getContext(), SongActivity.class);
+                            intent.putExtra("BOOK_NUMBER",bookNumber);
+                            intent.putExtra("SONG_NUMBER",songNumber);
+                            intent.putExtra("PRELUDE",true);
+                            startActivity(intent);
+
                         }
                     });
                     mGridLayout.addView(linearLayout);
@@ -172,14 +184,14 @@ public class MainActivity extends AppCompatActivity {
 
         int id;
         if(book == 0 && x == 8)
-            id = context.getResources().getIdentifier("joined", "drawable", context.getPackageName());
+            id = context.getResources().getIdentifier("ic_"+13, "drawable", context.getPackageName());
         else
             id = context.getResources().getIdentifier("ic_"+x, "drawable", context.getPackageName());
         return id;
     }
 
-    public static String getSongName(int x, int book){
-        switch(x){
+    public static String getSongName(int song, int book){
+        switch(song){
 
             case 1:return "C major";
             case 2:return "C minor";
@@ -188,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
             case 5:return "D major";
             case 6:return "D minor";
             case 7:return "E♭ major";
-            case 8: if(book == 0) return "E♭ minor \n/ D# minor"; else return "D# minor";
+            case 8: if(book == 0) return "E♭/D# minor"; else return "D# minor";
             case 9:return "E major";
             case 10:return "E minor";
             case 11:return "F major";
@@ -207,7 +219,7 @@ public class MainActivity extends AppCompatActivity {
             case 24:return "B minor";
 
         }
-        return ""+x;
+        return ""+song;
     }
 
 }
--- BachWTC2/app/src/main/java/gohar/mostafa/bachwtc/SongActivity.java
+++ BachWTC2/app/src/main/java/gohar/mostafa/bachwtc/SongActivity.java
@@ -1,26 +1,164 @@
 package gohar.mostafa.bachwtc;
 
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.pdf.PdfRenderer;
+import android.os.Build;
+import android.os.ParcelFileDescriptor;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayout;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SongActivity extends AppCompatActivity {
 
+    public int bookNumber;
+    public int songNumber;
+    public boolean isPrelude;
+    public static SongActivity songActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
         Intent intent= getIntent();
         Bundle b = intent.getExtras();
-        int bookNumber = (int) b.get("BOOK_NUMBER");
-        int songNumber = (int) b.get("SONG_NUMBER");
-        boolean isPrelude = (boolean) b.get("PRELUDE");
-        String Name = (String) b.get("NAME");
+        bookNumber = (int) b.get("BOOK_NUMBER");
+        songNumber = (int) b.get("SONG_NUMBER");
+        isPrelude = (boolean) b.get("PRELUDE");
+
+        songActivity = this;
 
 
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(bookNumber+"_"+songNumber+"_"+isPrelude+"_"+Name);
+//        TextView textView = findViewById(R.id.textView);
+//        textView.setText(bookNumber+"_"+songNumber+"_"+isPrelude+"_"+MainActivity.getSongName(songNumber,bookNumber));
+        ViewPager mViewPager;
+        SlidingTabLayout tabs;
 
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(new SongActivity.MyPagerAdapter(getSupportFragmentManager()));
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true);
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(mViewPager);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            SongActivity.MyFragment myFragment = SongActivity.MyFragment.getInstance(position);
+            return myFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return ""+(position+1);
+        }
+    }
+
+    // Instances of this class are fragments representing a single
+    // object in our collection.
+    public static class MyFragment extends Fragment {
+        public static SongActivity.MyFragment getInstance(int position){
+            SongActivity.MyFragment myFragment = new SongActivity.MyFragment();
+            Bundle args = new Bundle();
+            args.putInt("BOOK_NUMBER",SongActivity.getInstance().bookNumber);
+            args.putInt("SONG_NUMBER",SongActivity.getInstance().songNumber);
+            args.putBoolean("PRELUDE",SongActivity.getInstance().isPrelude);
+            args.putInt("position",position);
+            myFragment.setArguments(args);
+            return myFragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+            View layout = inflater.inflate(R.layout.pdf_page,container,false);
+
+            Bundle bundle = getArguments();
+            int bookNumber = bundle.getInt("BOOK_NUMBER");
+            int songNumber = bundle.getInt("SONG_NUMBER");
+            boolean isPrelude = bundle.getBoolean("PRELUDE");
+            int position = bundle.getInt("position");
+            PDFView pdfView = (PDFView) layout.findViewById(R.id.pdfView);
+            String song = songNumber+"";
+
+            String path;
+            if(position == 0)
+                path = "A/";
+            else
+                path = "B/";
+            if(songNumber < 10)
+                song = 0+song;
+            path +=bookNumber+"_"+song;
+            if(isPrelude)
+                path+="_p.pdf";
+            else
+                path+="_f.pdf";
+            pdfView.fromAsset(path)
+                    .enableSwipe(true) // allows to block changing pages using swipe
+                    .swipeHorizontal(false)
+                    .enableDoubletap(true)
+                    .defaultPage(0)
+                    .enableAntialiasing(true)
+                    .scrollHandle(new DefaultScrollHandle(getContext()))
+                    .load();
+
+            return layout;
+        }
+    }
+
+    public static SongActivity getInstance(){
+        return songActivity;
     }
 }
